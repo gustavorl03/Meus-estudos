cd9dbce1620ca73a7be0c5a95cc67fdc21474dca
Gustavo Lacerda <raphaelgustavo312@gmail.com>
2025-09-19 21:22:07 -0300

    avançando em python

--- Python/intro.py
+++ Python/intro.py
@@ -1,5 +1,27 @@
-X1= float(input("Digite um número: "))
-X2= float(input("Digite outro número: "))
-res= X1 + X2
-print(f"O resultado da soma entre {X1} e {X2} é igual a {res} ")
+X1= float(input("Digite o primeiro valor: "))
+X2= float(input("Digite o segundo valor: "))
+
+soma = X1 + X2
+subtra = X1 - X2
+multi = X1 * X2
+div= X1 / X2
+
+print ("Escolha a operação que deseja fazer:") 
+print("")
+print("1)SOMA")
+print("2)SUBTRAÇÃO")
+print("3)MULTIPLICAÇÃO")
+print("4)DIVISÃO")
+
+print("")
+resp= int(input(""))
+
+if (resp==1):
+    print(soma) 
+elif(resp==2):
+    print(subtra)
+elif(resp==3):
+    print(multi)
+else:
+    print(div)
 
