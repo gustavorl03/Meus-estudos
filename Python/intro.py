946050246d2f623534c9d9f36539b69447b830d6
gustavorl20 <raphaelgustavo312@gmail.com>
2025-09-29 09:53:54 -0300

    condições aninhadas em python

--- Python/intro.py
+++ Python/intro.py
@@ -4,5 +4,5 @@ print("Seu nome em maiúsculas é {}".format(nome.upper()))
 print("Seu nome em minúsculas é {}".format(nome.lower()))
 print("Seu nome ao todo tem {} letras".format(len(nome) - nome.count(" ")))
 print("Seu primeiro nome tem {} letras".format(nome.find(" ")))
-                                                                                        
 
+        
