cee4ce64211bc4f58baa41e66d28f3b7a50ccd9a
gustavorl20 <raphaelgustavo312@gmail.com>
2025-10-01 10:56:55 -0300

    estrutura while em python

---
+++ Python/repeticoeswhile.py
@@ -0,0 +1,27 @@
+#sexo = ""
+#sexo = str(input("Informe seu sexo: [M/F] ")).strip().#upper()[0]
+#while sexo not in "FfMm":
+#    sexo = str(input("Dados inválidos. Por favor, informe o seu sexo: [M/F] ")).strip().upper()[0]
+#print("Sexo {} registrado com sucesso!".format(sexo))
+
+#from random import randint
+#computador= randint(0,10)
+#tent = 0
+#tent1 = 0
+#tent2 = 0
+#print("Sou seu computador...")
+#print("Acabei de pensar em um número entre 0 e 10.")
+#print("Será que você consegue adivinhar qual foi?")
+#n1= int(input("Qual o seu palpite? "))
+#while n1 != computador:
+#    if computador > n1:
+#        print("Mais... Tente mais uma vez.")
+#        n1= int(input("Qual o seu palpite? "))
+#        tent1 = tent1 + 1
+#    else:
+#        print("Menos... Tente mais uma vez.")
+#        n1= int(input("Qual o seu palpite? "))
+#        tent2 = tent2 + 1   
+#tent = tent1 + tent2
+#print("Acertou com {} tentativas. Parabéns!".format(tent))
+
