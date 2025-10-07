9805c1494e73f4241942b163053433f3ab2c782a
Gustavo Lacerda <raphaelgustavo312@gmail.com>
2025-10-07 11:02:22 -0300

    começando com listas em python

---
+++ Python/listas.py
@@ -0,0 +1,55 @@
+#num = [2,5,9,1]
+#num[2] = 3
+#num.append(7)
+#num.sort(reverse=True)
+#num.insert(2,0)
+#num.pop(4)
+#if 5 in num:
+#    num.remove(5)
+#else:
+#    print("Não achei o número 5")
+#print(num)
+#print(f"Essa lista tem {len(num)} elementos! ")
+
+#valores = []
+#for cont in range(0,5):
+#    valores.append(int(input("Digite um valor: ")))
+#for c, v in enumerate(valores):
+#    print(f"Na posição {c + 1} encontrei o valor {v}")
+#print("Cheguei ao final da lista.")
+
+#a = [2,3,4,7]
+#b = a[:]
+#b[2] = 8
+#print(a)
+#print(b)
+
+#----------------------------------------------------------
+
+#valores = []
+#maiorvalor = 0
+#menorvalor = 0
+#posicaomaior = 0
+#posicaomenor = 0
+
+#for c in range(0,5):
+#    valores.append(int(input(f"Digite um valor para a Posição {c}: ")))
+#    if c == 0:
+#        maiorvalor = valores[c]
+#        menorvalor = valores[c]
+#    
+#    if valores[c] > maiorvalor:
+#        maiorvalor =  valores[c]
+#        posicaomaior = c
+#        
+#    if valores [c] < menorvalor:
+#        menorvalor = valores[c]
+#        posicaomenor = c
+# 
+#print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+#print(f"Você digitou os valores {valores}")
+#print(f"O maior valor digitado foi {maiorvalor} na posição {posicaomaior}...")
+#print(f"O menor valor digitado foi {menorvalor} na posição {posicaomenor}...")
+
+#----------------------------------------------------------
+
