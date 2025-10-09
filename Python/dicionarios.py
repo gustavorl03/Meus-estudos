d2742af8b5daa519dce4eca331b2d8c3d32e54f7
Gustavo Lacerda <raphaelgustavo312@gmail.com>
2025-10-09 10:21:25 -0300

    dicionários e funções em python

---
+++ Python/dicionarios.py
@@ -0,0 +1,6 @@
+nota = dict()
+nota["nome"] = str(input("Nome: "))
+nota["media"] = float(input(f"Média de {nota["nome"]}: "))
+if nota["media"] >= 7:    
+print("-=" * 30)
+
---
+++ Python/funções.py
@@ -0,0 +1,21 @@
+#def área(l, c):
+#    area = c * l
+#    print(f"A área de um terreno de {l} x {c} é de {area}m2. ")
+
+
+#print("Controle de Terrenos")
+#print("---------------------")
+#l = float(input("Largura (m): "))
+#c = float(input("Comprimento (m): "))
+#área(l, c)
+
+#def escreva(msg):
+#    tam = len(msg) + 4
+#    print("~" * tam)
+#    print(f"  {msg}")
+#    print("~" * tam)
+
+
+#escreva("Gustavo Lacerda")
+#escreva("Jujutsu Kaisen")
+#escreva("My Hero Academia")
